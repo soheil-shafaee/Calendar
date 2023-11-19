6e9fb35cba84f1b7b639fac644166a3e828c1562
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2023-11-19 15:58:24 +0330

    Attached Two Window The Calendar and Add Meeting File

--- calendar.py
+++ calendar.py
@@ -1,7 +1,9 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QCalendarWidget, QLabel, QLineEdit, QTextEdit
 from PyQt5 import uic
-from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5.QtGui import QIcon
 import sys
+
+from add_meeting import AddMeeting
 
 
 class UI(QMainWindow):
@@ -16,12 +18,27 @@ class UI(QMainWindow):
         self.calendar = self.findChild(QCalendarWidget, "calendarWidget")
         self.toDo = self.findChild(QLabel, "textDoLabel")
 
+        # Click The Button
+        self.plusButton.clicked.connect(self.usePlus)
+        self.calendar.selectionChanged.connect(self.usePlus)
+
         # Change Icon And Image Icon
         self.setWindowIcon(QIcon("icon/calendar.png"))
         self.plusButton.setIcon(QIcon("icon/plus.png"))
+        # Resize The Window
+        self.setFixedSize(580, 527)
+
+        # Selected Date
+        self.date = ""
 
         # Show The App
         self.show()
+
+    # Define Function To Use Second Window For Add Meeting
+    def usePlus(self):
+        self.window = AddMeeting()
+        data = self.calendar.selectedDate()
+        self.window.dateLabel.setText(str(data.selected.toString()))
 
 
 # Initialize The App
